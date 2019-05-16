1801785eb6c198c6733d86926dba00d12376148d
Gabrieli Felicetti Xavier <48328193+felicettixx@users.noreply.github.com>
2019-05-16 15:16:30 -0300

    Add files via upload

---
+++ untprincipal.pas
@@ -0,0 +1,26 @@
+unit untprincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+
+type
+  Tfrm1 = class(TForm)
+    pnl1: TPanel;
+    img1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frm1: Tfrm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
